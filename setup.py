711a707415727c8010be1aa614beb942f82f1e6a
wildlarva <wildlarva@gmail.com>
2019-12-23 21:59:10 +0900

    Fix the broken installation

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import sys
 
 from setuptools import find_packages, setup
 
-if sys.version_info < (3,8):
+if sys.version_info < (3, 8):
     sys.exit('Sorry, Python < 3.8 is not supported.')
 
 setup(
     name='mcdecoder',
     version='0.1a2.dev1',
-    packages=find_packages('src'),
+    packages=find_packages('src', exclude=['test', 'test.*', '*.test', '*.test.*']),
     package_dir={'': 'src'},
-    include_package_data=True,
+    package_data={'mcdecoder': ['templates/*']},
     python_requires='>=3.8',
     install_requires=['pyyaml', 'jinja2'],
 )
