f0ea89f5ca07e0aacc7c3f7dd4013b0126b3e86b
wildlarva <wildlarva@gmail.com>
2019-12-23 13:16:22 +0900

    Change the version status to development

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3,8):
 
 setup(
     name='mcdecoder',
-    version='0.1a1',
+    version='0.1a2.dev1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     package_data={
