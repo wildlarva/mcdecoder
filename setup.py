274917c4e4e8de7bf05b842aa2ebb02cfdb36c43
wildlarva <wildlarva@gmail.com>
2019-12-24 21:18:39 +0900

    Change the version to 0.1a2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 8):
 
 setup(
     name='mcdecoder',
-    version='0.1a2.dev1',
+    version='0.1a2',
     packages=find_packages(
         'src', exclude=['test', 'test.*', '*.test', '*.test.*']),
     package_dir={'': 'src'},
