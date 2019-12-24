9f48dc7f5f193c63c17702a319821cf3ccd500fe
wildlarva <wildlarva@gmail.com>
2019-12-24 17:55:19 +0900

    Refactor test code to increase readability

--- src/mcdecoder/test/test_app.py
+++ src/mcdecoder/test/test_app.py
@@ -1,17 +1,17 @@
-from mcdecoder import app
+from mcdecoder.app import run_app
 
 
 def test_run_app_without_arguments() -> None:
-    assert app.run_app(['mcdecoder']) == 2
-    assert app.run_app(['mcdecoder', 'generate']) == 2
+    assert run_app(['mcdecoder']) == 2
+    assert run_app(['mcdecoder', 'generate']) == 2
 
 
 def test_run_app_help() -> None:
-    assert app.run_app(['mcdecoder', '--help']) == 0
-    assert app.run_app(['mcdecoder', '-h']) == 0
-    assert app.run_app(['mcdecoder', 'generate', '--help']) == 0
-    assert app.run_app(['mcdecoder', 'generate', '-h']) == 0
+    assert run_app(['mcdecoder', '--help']) == 0
+    assert run_app(['mcdecoder', '-h']) == 0
+    assert run_app(['mcdecoder', 'generate', '--help']) == 0
+    assert run_app(['mcdecoder', 'generate', '-h']) == 0
 
 
 def test_run_app_generate() -> None:
-    assert app.run_app(['mcdecoder', 'generate', 'test/arm.yaml']) == 0
+    assert run_app(['mcdecoder', 'generate', 'test/arm.yaml']) == 0
