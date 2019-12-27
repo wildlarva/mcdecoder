a2d689cfe84f954db5feaece8c438d2736276507
wildlarva <wildlarva@gmail.com>
2019-12-27 20:30:17 +0900

    Format tests
    See also: #9, #10

--- src/mcdecoder/test/test_checker.py
+++ src/mcdecoder/test/test_checker.py
@@ -8,8 +8,9 @@ def test_check() -> None:
 
 def test__check_no_mask() -> None:
     assert len(list(_check('test/arm.yaml', 'e9 2d 48 00', 16))) == 0
-    
-    assert len(list(_check('test/arm.yaml', '1110 1001 0010 1101 0100 1000 0000 0000', 2))) == 0
+
+    assert len(
+        list(_check('test/arm.yaml', '1110 1001 0010 1101 0100 1000 0000 0000', 2))) == 0
 
 
 def test__check_one_character_mask() -> None:
@@ -20,8 +21,9 @@ def test__check_one_character_mask() -> None:
     assert errors1[0].type == 'undefined'
     assert errors1[0].bits_start == 0xf92d4800
     assert errors1[0].bits_end == 0xf92d4800
-    
-    assert len(list(_check('test/arm.yaml', 'x110 1001 0010 1101 0100 1000 0000 0000', 2))) == 0
+
+    assert len(
+        list(_check('test/arm.yaml', 'x110 1001 0010 1101 0100 1000 0000 0000', 2))) == 0
 
     # Halfway
     errors2 = list(_check('test/arm.yaml', 'ex 2d 48 00', 16))
@@ -34,13 +36,15 @@ def test__check_one_character_mask() -> None:
     assert errors2[1].type == 'undefined'
     assert errors2[1].bits_start == 0xea2d4800
     assert errors2[1].bits_end == 0xef2d4800
-    
-    assert len(list(_check('test/arm.yaml', '111x 1001 0010 1101 0100 1000 0000 000x', 2))) == 1
+
+    assert len(
+        list(_check('test/arm.yaml', '111x 1001 0010 1101 0100 1000 0000 000x', 2))) == 1
 
     # Last letter
     assert len(list(_check('test/arm.yaml', 'e9 2d 48 0x', 16))) == 0
-    
-    assert len(list(_check('test/arm.yaml', '1110 1001 0010 1101 0100 1000 0000 000x', 2))) == 0
+
+    assert len(
+        list(_check('test/arm.yaml', '1110 1001 0010 1101 0100 1000 0000 000x', 2))) == 0
 
 
 def test__check_sequential_mask() -> None:
@@ -58,8 +62,9 @@ def test__check_sequential_mask() -> None:
     assert errors[2].type == 'undefined'
     assert errors[2].bits_start == 0xe93d4800
     assert errors[2].bits_end == 0xeffd4800
-    
-    assert len(list(_check('test/arm.yaml', '111x x001 0010 1101 0100 1000 0000 000x', 2))) == 2
+
+    assert len(
+        list(_check('test/arm.yaml', '111x x001 0010 1101 0100 1000 0000 000x', 2))) == 2
 
 
 def test__check_split_mask() -> None:
@@ -73,5 +78,6 @@ def test__check_split_mask() -> None:
     assert errors[1].type == 'undefined'
     assert errors[1].bits_start == 0xe92e4800
     assert errors[1].bits_end == 0xef2f4800
-    
-    assert len(list(_check('test/arm.yaml', '111x 100x 0010 1101 0100 1000 0000 000x', 2))) == 2
+
+    assert len(
+        list(_check('test/arm.yaml', '111x 100x 0010 1101 0100 1000 0000 000x', 2))) == 2
