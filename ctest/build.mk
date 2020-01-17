b35e4e1c731e545a3a8a3e54424da78e8750d920
wildlarva <wildlarva@gmail.com>
2020-01-18 08:11:49 +0900

    Refactor ctest for maintainability

--- ctest/build.mk
+++ ctest/build.mk
@@ -1,37 +1,40 @@
-include build/conanbuildinfo.mak
+BUILD_DIR = build
+
+include $(BUILD_DIR)/conanbuildinfo.mak
 
 #----------------------------------------
 #	 Make variables
 #----------------------------------------
 
-CXX_SRCS = \
-	test.cpp \
-	stub.c \
-	out/arm_mcdecoder.c \
-	out/ab_mcdecoder.c \
-	out/at_mcdecoder.c \
-	out/atb_mcdecoder.c \
-	out/riscv_mcdecoder.c \
-	out/pc_mcdecoder.c \
-	out/cc_mcdecoder.c \
-	out/dt16x2_mcdecoder.c \
-	out/dt32x1_mcdecoder.c
+COMMON_TEST_DIR = ../test
+
+DECODER_DIR = out
+
+DECODER_SRCS = \
+	$(DECODER_DIR)/arm_mcdecoder.c \
+	$(DECODER_DIR)/ab_mcdecoder.c \
+	$(DECODER_DIR)/at_mcdecoder.c \
+	$(DECODER_DIR)/atb_mcdecoder.c \
+	$(DECODER_DIR)/riscv_mcdecoder.c \
+	$(DECODER_DIR)/pc_mcdecoder.c \
+	$(DECODER_DIR)/cc_mcdecoder.c \
+	$(DECODER_DIR)/dt16x2_mcdecoder.c\
+	$(DECODER_DIR)/dt32x1_mcdecoder.c
+
+CSRCS = \
+	stub.c
+
+CSRCS += $(DECODER_SRCS)
+
+CPP_SRCS = \
+	test.cpp
 
 CXX_OBJ_FILES = \
-	build/test.o \
-	build/stub.o \
-	build/arm_mcdecoder.o \
-	build/ab_mcdecoder.o \
-	build/at_mcdecoder.o \
-	build/atb_mcdecoder.o \
-	build/riscv_mcdecoder.o \
-	build/pc_mcdecoder.o \
-	build/cc_mcdecoder.o \
-	build/dt16x2_mcdecoder.o \
-	build/dt32x1_mcdecoder.o
+	$(patsubst %.c, $(BUILD_DIR)/%.o, $(notdir $(CSRCS))) \
+	$(patsubst %.cpp, $(BUILD_DIR)/%.o, $(notdir $(CPP_SRCS)))
 
 EXE_FILENAME = \
-	build/test
+	$(BUILD_DIR)/test
 
 
 #----------------------------------------
@@ -51,30 +54,36 @@ LDLIBS += $(addprefix -l, $(CONAN_LIBS))
 #	 Make Commands
 #----------------------------------------
 
-COMPILE_CXX_COMMAND ?= \
+COMPILE_CXX_COMMAND = \
 	g++ -c $(CPPFLAGS) $(CXXFLAGS) $< -o $@
 
-COMPILE_C_COMMAND ?= \
+COMPILE_C_COMMAND = \
 	gcc -c $(CFLAGS) $< -o $@
 
-CREATE_EXE_COMMAND ?= \
+CREATE_EXE_COMMAND = \
 	g++ $(CXX_OBJ_FILES) \
 	$(CXXFLAGS) $(LDFLAGS) $(LDLIBS) \
 	-o $(EXE_FILENAME)
+
+CREATE_DECODER_COMMAND = \
+	mcdecoder generate --output $(DECODER_DIR) $<
 
 
 #----------------------------------------
 #	 Make Rules
 #----------------------------------------
 
-.PHONY: all clean exe test
+.PHONY: all clean exe generate test
 
 all: exe
 
-exe: $(EXE_FILENAME)
+generate: $(DECODER_SRCS)
+
+exe: generate $(EXE_FILENAME)
 
 clean:
 	rm -vf $(EXE_FILENAME) $(CXX_OBJ_FILES)
+	rm -vfr $(DECODER_DIR)
 
 test: exe
 	$(EXE_FILENAME)
@@ -82,11 +91,38 @@ test: exe
 $(EXE_FILENAME) : $(CXX_OBJ_FILES)
 	$(CREATE_EXE_COMMAND)
 
-build/%.o: %.cpp
+$(BUILD_DIR)/%.o: %.cpp
 	$(COMPILE_CXX_COMMAND)
 
-build/%.o: %.c
+$(BUILD_DIR)/%.o: %.c
 	$(COMPILE_C_COMMAND)
 
-build/%.o: out/%.c
+$(BUILD_DIR)/%.o: $(DECODER_DIR)/%.c
 	$(COMPILE_C_COMMAND)
+
+$(DECODER_DIR)/arm_mcdecoder.c: $(COMMON_TEST_DIR)/arm.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/ab_mcdecoder.c: $(COMMON_TEST_DIR)/arm_big.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/at_mcdecoder.c: $(COMMON_TEST_DIR)/arm_thumb.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/atb_mcdecoder.c: $(COMMON_TEST_DIR)/arm_thumb_big.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/riscv_mcdecoder.c: $(COMMON_TEST_DIR)/riscv.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/pc_mcdecoder.c: $(COMMON_TEST_DIR)/primitive_condition.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/cc_mcdecoder.c: $(COMMON_TEST_DIR)/complex_condition.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/dt16x2_mcdecoder.c: $(COMMON_TEST_DIR)/decision_tree_code16x2.yaml
+	$(CREATE_DECODER_COMMAND)
+
+$(DECODER_DIR)/dt32x1_mcdecoder.c: $(COMMON_TEST_DIR)/decision_tree_code32x1.yaml
+	$(CREATE_DECODER_COMMAND)
