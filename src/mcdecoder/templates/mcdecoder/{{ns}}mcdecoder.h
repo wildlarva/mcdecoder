07be70389e1728e87d791872c87c0aa0bcaebdaa
wildlarva <wildlarva@gmail.com>
2020-01-21 21:24:35 +0900

    Add API comment

--- src/mcdecoder/templates/mcdecoder/{{ns}}mcdecoder.h
+++ src/mcdecoder/templates/mcdecoder/{{ns}}mcdecoder.h
@@ -9,34 +9,39 @@
  * Types
  */
 
+/** Decoding request */
 typedef struct {
-	const uint8_t *codes;
+	const uint8_t *codes;	/** Codes to be input */
 } {{ ns }}DecodeRequest;
 
+/** Instruction id to identify a decoded instruction */
 typedef enum {
 	{% for inst in instruction_decoders -%}
-		{{ ns }}InstructionId_k_{{ inst.name }},
+		{{ ns }}InstructionId_k_{{ inst.name }},	/** {{ inst.name }} */
 	{% endfor %}
-	{{ ns }}InstructionId_kUnknown,
+	{{ ns }}InstructionId_kUnknown, /** Unknown instruction */
 } {{ ns }}InstructionId;
 
+/** Number of instruction ids */
 #define {{ ns }}INSTRUCTION_ID_MAX {{ ns }}InstructionId_kUnknown
 
 {% for inst in instruction_decoders %}
-typedef struct {
-	{% for field in inst.field_decoders -%}
-		uint{{ field.type_bit_size }}_t {{ field.name }};	/* {% for sf in field.subfield_decoders %}{{ sf.start_bit_in_instruction }}-{{ sf.end_bit_in_instruction }}{% if not loop.last %}, {% endif %}{% endfor %} */
-	{% endfor %}
-} {{ ns }}InstructionDecodeResult_{{ inst.name }};
+	/** Decoding result for {{ inst.name }} */
+	typedef struct {
+		{% for field in inst.field_decoders -%}
+			uint{{ field.type_bit_size }}_t {{ field.name }};	/** Bit {% for sf in field.subfield_decoders %}{{ sf.start_bit_in_instruction }}-{{ sf.end_bit_in_instruction }}{% if not loop.last %}, {% endif %}{% endfor %}: Decoding result for {{ field.name }} */
+		{% endfor %}
+	} {{ ns }}InstructionDecodeResult_{{ inst.name }};
 {% endfor %}
 
+/** Decoding result */
 typedef struct {
-	{{ ns }}InstructionId instruction_id;
+	{{ ns }}InstructionId instruction_id;	/** Decoded instruction id */
     union {
 		{% for inst in instruction_decoders -%}
-        	{{ ns }}InstructionDecodeResult_{{ inst.name }} {{ inst.name }};
+        	{{ ns }}InstructionDecodeResult_{{ inst.name }} {{ inst.name }};	/** Decoding result for {{ inst.name }} */
 		{% endfor %}
-    } instruction;
+    } instruction;	/** Decoding result for an instruction */
 } {{ ns }}DecodeResult;
 
 
@@ -44,6 +49,12 @@ typedef struct {
  * Functions
  */
 
+/**
+ * Decode an instruction
+ *
+ * @param request decoding request
+ * @param result decoding result
+ */
 extern bool {{ ns }}decode_instruction(const {{ ns }}DecodeRequest *request, {{ ns }}DecodeResult *result);
 
 
